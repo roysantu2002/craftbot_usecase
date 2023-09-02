93a366d3a7bb6bfb6d0876e19130a1a1ca9e30e3
Santu Roy <roysantu2002@yahoo.com>
2023-09-02 22:13:24 +0530

    modified logs

--- scripts/urls.py
+++ scripts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import NetworkDeviceInfoCreateAPIView, NetworkDeviceInfoListView, NetworkDeviceInfoDetailView
+from .views import  NetworkDeviceLogListCreateAPIView, NetworkDeviceLogRetrieveAPIView, NetworkDeviceInfoCreateAPIView, NetworkDeviceInfoListView, NetworkDeviceInfoDetailView, NetworkDeviceLogCreateAPIView
 
 urlpatterns = [
-    path('api/network-devices/', NetworkDeviceInfoCreateAPIView.as_view(), name='network-device-create'),
-    path('api/network-devices/list/', NetworkDeviceInfoListView.as_view(), name='network-device-list'),
-    path('api/network-devices/<int:pk>/', NetworkDeviceInfoDetailView.as_view(), name='network-device-detail'),
+    path('network-devices/', NetworkDeviceInfoCreateAPIView.as_view(), name='network-device-create'),
+    path('network-devices/list/', NetworkDeviceInfoListView.as_view(), name='network-device-list'),
+    path('network-devices/<int:pk>/', NetworkDeviceInfoDetailView.as_view(), name='network-device-detail'),
+    path('logs/create/', NetworkDeviceLogCreateAPIView.as_view(), name='networkdevice-log-create'),
+    path('logs/', NetworkDeviceLogListCreateAPIView.as_view(), name='networkdevice-log-list'),
+    path('logs/<int:pk>/', NetworkDeviceLogRetrieveAPIView.as_view(), name='networkdevice-log-detail'),
 ]
 
--- scripts/views.py
+++ scripts/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import NetworkDeviceInfo
+from .models import NetworkDeviceInfo, NetworkDeviceLog
 from .serializers import NetworkDeviceInfoSerializer, NetworkDeviceLogSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import generics
 
 class NetworkDeviceLogCreateAPIView(APIView):
     serializer_class = NetworkDeviceLogSerializer
@@ -17,6 +18,15 @@ class NetworkDeviceLogCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class NetworkDeviceLogListCreateAPIView(generics.ListCreateAPIView):
+    queryset = NetworkDeviceLog.objects.all()
+    serializer_class = NetworkDeviceLogSerializer
+
+class NetworkDeviceLogRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = NetworkDeviceLog.objects.all()
+    serializer_class = NetworkDeviceLogSerializer
+    lookup_field = 'pk'  # Use 'pk' (primary key) for retrieving by ID
+    
 class NetworkDeviceInfoCreateAPIView(APIView):
     serializer_class = NetworkDeviceInfoSerializer
     def post(self, request, format=None):
